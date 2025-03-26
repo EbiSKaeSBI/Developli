d8f6ccd5af1dda9bf9d28598ffc90fdb1bace349
Vladimir <zajtzeffvl4d@yandex.ru>
2025-03-27 00:49:34 +0300

    add settings tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ function App() {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
+      <h1 className="text-3xl font-bold font-readex-pro text-center">
         Hello world!
       </h1>
     </>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,53 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "readex-pro": ["Readex Pro", "sans-serif"],
+      },
+      colors: {
+        // Primary colors
+        "green-1": "#2EB873",
+        "green-2": "#47D18C",
+        "green-3": "#B1DFC9",
+        "blue-1": "#0075EB",
+        "blue-2": "#FACCB9",
+        "blue-3": "#FCDDD1",
+        "orange-1": "#F59A74",
+        "orange-2": "#FACCB9",
+        "orange-3": "#FCDDD1",
+        // Secondary colors
+        "yellow": "#FEE5A4",
+        "yellow-green": "#EAEDB6",
+        "light-blue": "#A4D8FE",
+        "purple": "#ABA4FE",
+        "pink": "#FEA4E5",
+        // seBlack
+        "seBlack-1": "#FEA4E5",
+        "seBlack-2": "#2A2F46",
+        "seBlack-3": "#30364F",
+        // Black
+        "black-1": "#1B2124",
+        "black-2": "#8D9092",
+        "black-3": "#C6C7C8",
+        // Neutrals
+        "grey-1": "#7F879E",
+        "grey-2": "#AAAFBE",
+        "grey-3": "#D4D7DF",
+        "L-gray-1": "#F3F3F3",
+        "L-gray-2": "#F6F8F9",
+        "L-gray-3": "#F9F9F9",
+        "white": "#FFFFFF",
+        "beige": "#FAFAFA",
+        // Alter Color
+        "red-1": "#E60019",
+        "red-2": "#AAAFBE",
+        "red-3": "#D4D7DF",
+        "Al-green-1": "#3D5BF6",
+        "Al-green-2": "#78C58C",
+        "Al-green-3": "#BBE2C6",
+      }
+    },
   },
   plugins: [],
 }
